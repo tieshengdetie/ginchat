0f95135e18d5d693325f6a85e200c8ed4feb67af
zhaojinsheng <123844231@qq.com>
2024-02-12 21:32:10 +0800

    sssssssf

--- test/testStudy.go
+++ test/testStudy.go
@@ -1,26 +1,17 @@
 package main
 
-import "fmt"
-
 func main() {
-	//// 对数组做切片
-	//array := [3]int{1, 2, 3} // array是数组
-	//slice3 := array[1:3]     // slice3是切片
-	//fmt.Println("slice3 type:", reflect.TypeOf(slice3))
-	//fmt.Println("slice3=", slice3) // slice3= [2 3]
-	//a := []int{1, 2}
-	//b := append(a, 3)
-	//
-	//c := append(b, 4)
-	//d := append(b, 5)
-	//
-	//fmt.Println(a, b, c[3], d[3])
-	s := []int{1, 2}
-	s = append(s, 4, 5, 6)
-	//s = append(s, 4)
-	//s = append(s, 5)
-	//s = append(s, 6)
-	fmt.Println(s)
-	fmt.Println(len(s), cap(s))
+	var y = []string{"A", "B", "C", "D"} //y的长度为4 cap 为4
+	var x = y[:3]                        //x的长度为3 cap 为4
+	//var d = x[0:3]                       //d的长度为3 cap 为4
+	//x = append(x, "Z")                   //此时x的长度为4 cap为4 因为y的长度同样为4 且底层指向的是同一个数组，所以y也改变了
+	//fmt.Println(x)                       //而因为d的长度没有改变所以还是 [A B c]
+	//fmt.Println(y)
+	//fmt.Println(d)
+	for i, s := range x {
+		print(i, s, ",")
 
+		x = append(x, "Z")
+		x[i+1] = "Z"
+	}
 }
