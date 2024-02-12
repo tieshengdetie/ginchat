6470086ce6a6360766fda54cbe4f1cd274dc666a
zhaojinsheng <123844231@qq.com>
2024-02-12 20:41:28 +0800

    d

--- test/testStudy.go
+++ test/testStudy.go
@@ -2,25 +2,24 @@ package main
 
 import "fmt"
 
-func main() {
-	//// 对数组做切片
-	//array := [3]int{1, 2, 3} // array是数组
-	//slice3 := array[1:3]     // slice3是切片
-	//fmt.Println("slice3 type:", reflect.TypeOf(slice3))
-	//fmt.Println("slice3=", slice3) // slice3= [2 3]
-	//a := []int{1, 2}
-	//b := append(a, 3)
-	//
-	//c := append(b, 4)
-	//d := append(b, 5)
-	//
-	//fmt.Println(a, b, c[3], d[3])
-	s := []int{1, 2}
-	s = append(s, 4, 5, 6)
-	//s = append(s, 4)
-	//s = append(s, 5)
-	//s = append(s, 6)
-	fmt.Println(s)
-	fmt.Println(len(s), cap(s))
+func fibonacci(mychan chan int) {
+	n := cap(mychan)
+	x, y := 1, 1
+	for i := 0; i < n; i++ {
+		mychan <- x
+		x, y = y, x+y
+	}
+	// 记得 close 信道
+	// 不然主函数中遍历完并不会结束，而是会阻塞。
+	close(mychan)
+}
 
+func main() {
+	pipline := make(chan int, 10)
+
+	go fibonacci(pipline)
+
+	for k := range pipline {
+		fmt.Println(k)
+	}
 }
