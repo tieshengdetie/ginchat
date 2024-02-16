354d83e03ee0fe934129c8adb6e36cae9ac60479
zhaojinsheng <123844231@qq.com>
2024-02-16 21:22:57 +0800

    pc

--- test/testStruct.go
+++ test/testStruct.go
@@ -1,5 +1,16 @@
 package main
 
 func main() {
+	switch f() {
+	case true:
+		println(1)
+	case false:
+		println(0)
+	default:
+		println(-1)
+	}
+}
 
+func f() bool {
+	return false
 }
---
+++ test/testmianshi.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int, 1)
+	c <- 1
+	close(c)
+	close(c)
+	fmt.Println("OK")
+}
